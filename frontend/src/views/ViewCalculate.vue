e467d54bfa759832e06cbfeefe077244e64b4f6b
Dmitry Podkovyrkin <dmitryrw@gmail.com>
2023-06-02 21:05:13 +0500

    frontend css

--- frontend/src/views/ViewCalculate.vue
+++ frontend/src/views/ViewCalculate.vue
@@ -15,19 +15,3 @@ import ComponentNextPoint from '@/components/ComponentNextPoint.vue';
 
 document.title = 'Profile radio planner';
 </script>
-
-<style>
-.grid {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-  grid-gap: 1rem;
-}
-.grid > div {
-  background: #EDE7F6;
-  padding: 1.5rem;
-  border-radius: 1rem;
-}
-input {
-  width: 50%;
-}
-</style>
