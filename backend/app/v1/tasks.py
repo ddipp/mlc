4ab10f1e292928121cf9ffc12d4655d540ae2d72
Dmitry Podkovyrkin <dmitryrw@gmail.com>
2023-07-17 16:55:31 +0900

    rq job for profile

--- backend/app/v1/tasks.py
+++ backend/app/v1/tasks.py
@@ -1,7 +1,47 @@
-from lib import GeoPoint
+import matplotlib.pyplot as plt
+
+from lib import GeoPoint, GeoProfile
 
 
 COEFF = 1000000
+
+
+def profile(la_a, lo_a, la_b, lo_b, cache_dir):
+    filename = "{0}_{1}_{2}_{3}.png".format(la_a, lo_a, la_b, lo_b)
+    (la_a, lo_a, la_b, lo_b) = (la_a / COEFF, lo_a / COEFF, la_b / COEFF, lo_b / COEFF)
+    p_a = GeoPoint(la_a, lo_a)
+    p_b = GeoPoint(la_b, lo_b)
+
+    distance = p_a.distance_to(p_b) / 1000
+    az_a_b = p_a.azimuth(p_b)
+    az_b_a = p_b.azimuth(p_a)
+
+    profile = GeoProfile(p_a, p_b)
+    profile_chart = profile.get_chart_data()
+    plt.rcParams["figure.figsize"] = (14, 9)
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.grid(True)
+    ax.plot(profile_chart['distance'], profile_chart['relief'], label="Elevation", linestyle='solid', linewidth=0.5)
+    ax.fill_between(profile_chart['distance'], profile_chart['relief'], min(profile_chart['relief']), color='darkgreen', alpha=.4)
+
+    ax.set_xlabel('Distance (m)')
+    ax.set_ylabel('Elevation (m)')
+
+    plt.legend()
+    plt.title(
+        f'Profile from "{profile.startpoint.name}" {profile.startpoint.latitude} {profile.startpoint.longitude} '
+        f'to "{profile.stoppoint.name}" {profile.stoppoint.latitude} {profile.stoppoint.longitude}\n'
+        f'Distance {profile.length / 1000:.2f} km')
+    plt.savefig(cache_dir / filename, dpi=300, format='png')
+    plt.close()
+    return {'distance': distance,
+            'az_a_b': az_a_b,
+            'az_b_a': az_b_a,
+            'p_a_elevation': p_a.elevation,
+            'p_b_elevation': p_b.elevation,
+            'filename': filename,
+            }
 
 
 def nextpoint(latitude_a, longitude_a, distance, bearing):
--- backend/app/v1/views.py
+++ backend/app/v1/views.py
@@ -3,7 +3,7 @@ import base64
 import matplotlib.pyplot as plt
 
 from rq.job import Job
-from flask import Blueprint, jsonify, current_app
+from flask import Blueprint, jsonify, current_app, send_file
 
 
 from lib import GeoPoint, GeoProfile
@@ -12,6 +12,38 @@ from lib import GeoPoint, GeoProfile
 v01 = Blueprint('v1', __name__)
 
 COEFF = 1000000
+
+
+@v01.route('/get_profile/<string:filename>')
+def get_profile(filename):
+    return send_file(current_app.config['CACHE_DIR_PROFILE'] / filename, mimetype='image/png')
+
+
+@v01.route('profile_add_task/<int(signed=True):la_a>/<int(signed=True):lo_a>/<int(signed=True):la_b>/<int(signed=True):lo_b>')
+def profile_add_task(la_a, lo_a, la_b, lo_b):
+    rq_job = current_app.task_queue.enqueue('app.v1.tasks.profile', la_a, lo_a, la_b, lo_b)
+    return jsonify(job_id=rq_job.id,
+                   job_status=rq_job.get_status(),
+                   job_result=rq_job.result)
+
+
+@v01.route('profile_check/<string:job_id>')
+def profile_check(job_id):
+    try:
+        job = Job.fetch(job_id, connection=current_app.redis)
+        job_status = job.get_status()
+        job_result = job.result
+        return jsonify(job_status=job_status,
+                       distance="{:.3f}".format(job_result['distance']),
+                       az_a_b="{:.2f}".format(job_result['az_a_b']),
+                       az_b_a="{:.2f}".format(job_result['az_b_a']),
+                       p_a_elevation=job_result['p_a_elevation'],
+                       p_b_elevation=job_result['p_b_elevation'],
+                       filename=job_result['filename'],
+                       )
+    except Exception:
+        job_status = None
+        return jsonify(job_status=job_status)
 
 
 @v01.route('nextpoint_check/<string:job_id>')
