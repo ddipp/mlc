4e27f1d9eda62443173a519dab3cd3ea7359f061
Dmitry Podkovyrkin <dmitryrw@gmail.com>
2023-12-13 18:48:47 +0500

    Fix. longitude from -180 to 180

--- app/v01/forms.py
+++ app/v01/forms.py
@@ -4,13 +4,13 @@ from wtforms import DecimalField, validators
 
 class DistanceForm(FlaskForm):
     latitude_a = DecimalField("Latitude", [validators.InputRequired(), validators.NumberRange(min=-90, max=90)])
-    longitude_a = DecimalField("Longitude", [validators.InputRequired(), validators.NumberRange(min=-90, max=90)])
+    longitude_a = DecimalField("Longitude", [validators.InputRequired(), validators.NumberRange(min=-180, max=180)])
     latitude_b = DecimalField("Latitude", [validators.InputRequired(), validators.NumberRange(min=-90, max=90)])
-    longitude_b = DecimalField("Longitude", [validators.InputRequired(), validators.NumberRange(min=-90, max=90)])
+    longitude_b = DecimalField("Longitude", [validators.InputRequired(), validators.NumberRange(min=-180, max=180)])
 
 
 class NextPointForm(FlaskForm):
     latitude = DecimalField("Latitude", [validators.InputRequired(), validators.NumberRange(min=-90, max=90)])
-    longitude = DecimalField("Longitude", [validators.InputRequired(), validators.NumberRange(min=-90, max=90)])
+    longitude = DecimalField("Longitude", [validators.InputRequired(), validators.NumberRange(min=-180, max=180)])
     distance = DecimalField("Distance (km)", [validators.DataRequired(), validators.NumberRange(min=0, max=40000)])
     bearing = DecimalField("Bearing (°)", [validators.InputRequired(), validators.NumberRange(min=-360, max=360)])
