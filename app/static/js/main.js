d2b86649b319fff4fe4a2e5a5d698b3ae6f88e0a
Dmitry Podkovyrkin <dmitryrw@gmail.com>
2023-12-19 23:14:18 +0500

    Ajax errors

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
-	$("#form").on("submit", function(event){
-		event.preventDefault();
-		const form_data = new FormData(this);
-		$("#answer").html("");
-		$.ajax({
-			type: "POST",
-			url: form_data.get("url"),
-			data: form_data,
-			processData: false,
-    		contentType: false,
-    		success:function(data, textStatus, jqXHR){
-    			$("#answer").html(data);
-		    },
-		});
-	});
+  $("#form").on("submit", function(event){
+    event.preventDefault();
+    const form_data = new FormData(this);
+    $("#answer").html("");
+    $.ajax({
+      type: "POST",
+      url: form_data.get("url"),
+      data: form_data,
+      processData: false,
+      contentType: false,
+      success:function(data){
+        $("#answer").html(data);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error(textStatus, errorThrown);
+        $("#answer").html(textStatus + errorThrown);
+      },
+    });
+  });
 });
